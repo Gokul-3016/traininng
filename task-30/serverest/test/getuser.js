99e890874bb264838793425338cd873f49c3ee6d
Gokul-3016 <gokulnath9594@gmail.com>
2021-06-01 11:19:26 +0530

    task30.05.2021

---
+++ task-30/serverest/test/getuser.js
@@ -0,0 +1,20 @@
+import supertest from "supertest"
+import endpoint from "../endpoint"
+import chai from "chai"
+import { expect } from "chai"
+import jsonschema1 from "../jsonschema/getschema.json"
+import { it } from "mocha"
+chai.use(require('chai-json-schema'));
+
+const url =supertest(endpoint.baseurl);
+
+describe('get users from the endpoints', () => {
+    it('get user details in the id', (done) => {
+        const res =  url.get(endpoint.getuser)
+        done()
+        .expect(200)
+        .expect((res) => {
+        expect(res.body).to.be.equal(jsonschema1.valid_schema)
+                });
+        });
+    });
